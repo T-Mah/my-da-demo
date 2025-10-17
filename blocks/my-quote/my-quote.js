d7d010ecd61bffc64210c3ec31df732227076b9e
T-Mah <76528937+T-Mah@users.noreply.github.com>
2025-10-17 13:37:24 -0600

    added code to my-quote

--- blocks/my-quote/my-quote.js
+++ blocks/my-quote/my-quote.js
@@ -1,4 +1,21 @@
 export default async function decorate(block) {
-    const quotes = [...block.firstElementChild.children];
-    block.classList.add(`my-quote-${quotes.length}-my-quotes`);
+    const [quotation, quoteAuthor] = [...block.children].map((c) => c.firstElementChild);
+    const quoteblock = document.createElement('quoteblock');
+    block.classList.add(`my-quote-${quotes.length}`);
+
+    quotation.className = 'my-quote-quotation';
+    quoteblock.append(quotation);
+
+    if (quoteAuthor) {
+    quoteAuthor.className = 'my-quote-attribution';
+    quoteblock.append(quoteAuthor);
+    const ems = quoteAuthor.querySelectorAll('em');
+    ems.forEach((em) => {
+      const cite = document.createElement('cite');
+      cite.innerHTML = em.innerHTML;
+      em.replaceWith(cite);
+    });
+  }
+  block.innerHTML = '';
+  block.append(quoteAuthor);
 }
